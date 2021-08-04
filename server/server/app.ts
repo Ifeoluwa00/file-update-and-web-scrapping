735c45ab64257336f62cdc812b05aa4d72df76a1
olukayode ifeoluwa <ifeoluwajoshua2015@gmail.com>
2021-08-04 01:30:41 +0100

    finished week5 task

--- server/server/app.ts
+++ server/server/app.ts
@@ -1,13 +1,40 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
+import controller from './controllers/productcontroller';
+  
 /*
 implement your server code here
 */
 
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    if (req.method === "GET") {
-      res.end(JSON.stringify({ name: "hello" }));
+  console.log(req.url,  req.url?.split("/").slice(-1)[0]);
+  if (req.url === "/api/products" && req.method === "GET") {
+      controller.getProducts(req, res)
+  }
+  else if (req.url?.split("/").slice(-1)[0] && req.method === "GET") {
+    console.log("valid id")
+    const id = req.url.split("/").slice(-1)[0];
+    controller.getProduct(req, res, +id);
+
+  } else if (req.url==="/api/products" && req.method=== "POST") {
+    controller.createProduct(req, res)
+  }
+    
+  else if (req.url?.split("/").slice(-1)[0] && req.method === "PUT") {
+    const id = req.url.split("/").slice(-1)[0];
+    controller.updateProduct(req, res, +id)
+  }
+    
+  else if (req.url?.split("/").slice(-1)[0] && req.method === "DELETE") {
+    const id = req.url.split("/").slice(-1)[0];
+    controller.deleteProduct(req, res, +id)
+  }  
+    
+  else {
+    res.writeHead(404, { "Content-Type": "application/json" })
+    res.end(JSON.stringify({ message: "Route Not Found"})) 
     }
   }
 );
-
-server.listen(3005);
+const PORT= process.env.PORT || 3005
+server.listen(3005, ()=> console.log(`Server running on port ${PORT}`)); 
+ 
--- server2/server2/app.ts
+++ server2/server2/app.ts
@@ -1,14 +1,58 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
+import fs from 'fs'
+import  request  from "request";
+import cheerrio from "cheerio";
+import jsdom from "jsdom"
+import url from 'url';
 /*
 implement your server code here
 */
+const { JSDOM } = jsdom
+
+
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     if (req.method === "GET") {
-      res.end(JSON.stringify({ name: "hello" }));
+      const urlObj = new url.URL(req.url as string, `http://${req.headers.host}`);
+      const urlToParse = urlObj.searchParams.get('url');
+      console.log(urlToParse);
+
+      if (urlToParse) {
+        request.get( urlToParse, (err, response, body) => {
+          if (!err && response.statusCode == 200) {
+            console.log(typeof body)
+           
+            let dom = new JSDOM(body);
+            let imgUrls = Array.from(dom.window.document.querySelectorAll('img')).map(img => img?.getAttribute('src')).filter(imgurl =>  imgurl?.toString() );
+            let title = dom.window.document.querySelector('title')?.textContent;
+            let description = dom.window.document.querySelector('meta[name="description"]')
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({
+              title,
+              description,
+              imgUrls
+            }, null, " "))
+          }else{
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({
+              status: 'error',
+              message: 'Unable to find the website.'
+            }, null, " "));
+        
+          }
+        })
+      }else{
+
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({
+          status: 'error',
+          message: 'Invalid url'
+        }, null, " "));
+      }
+      
     }
   }
 );
-
-server.listen(3001);
+const port = 3001;
+server.listen( port, ()=>{ console.log(`listening on ${port}`)});
